c284211eb9f688f5128c77005abe44429c1d6b8f
Thej <tkodiyat01@gmail.com>
2017-10-13 20:03:46 -0700

    initial commit - mostly finished but have some bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = new User();
+  users = [];
+  onSubmit(){
+    this.users.push(this.user);
+    console.log(this.user)
+    this.user = new User();
+  }
 }
