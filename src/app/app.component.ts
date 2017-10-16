29eb7c657b3a73f6fd7c8b43a58f46aeee24a91f
Thej <tkodiyat01@gmail.com>
2017-10-16 13:45:44 -0700

    displays submit message, fixed streetaddress  and updated confirm password issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,20 @@ import { User } from './user';
 export class AppComponent {
   user = new User();
   users = [];
+  keys = [];
+  bool = false
+
+
+  display() {
+    this.keys = (Object.keys(this.users[0]))
+    this.bool = true
+  }
   onSubmit(){
     this.users.push(this.user);
     console.log(this.user)
+    console.log(this.users[0])
     this.user = new User();
+    this.display()
+    console.log(this.keys)
   }
 }
